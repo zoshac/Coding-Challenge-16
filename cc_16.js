6bf2dd0f05c0a4bae19286ee77c9b919e614569d
Zosha Chung <czosha@gmail.com>
2025-03-29 14:56:59 -0400

    Task 2- Implement fetchProductsThen using .then() and .catch()

--- cc_16.js
+++ cc_16.js
@@ -1,1 +1,18 @@
-// Task 2
+// Task 2 Fetching Products 
+function fetchProductsThen() {
+    fetch("https://www.course-api.com/javascript-store-products")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Unable to load");
+        }
+        return response.json();
+    })
+    .then(data => {
+        data.forEach(product => {
+            console.log(fetchProductsThen.fields.name);
+        });
+    })
+    .catch(error => {
+        console.error("Fetch error:", error);
+    });
+}
