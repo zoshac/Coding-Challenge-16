7f422ce7c5dfa27904da442106faffcd4283528f
Zosha Chung <czosha@gmail.com>
2025-03-29 15:23:37 -0400

    Task 5- Display products in the DOM with name, price, and image

--- cc_16.js
+++ cc_16.js
@@ -32,3 +32,36 @@ async function fetchProductsAsync() {
         handleError(error);
     }
 }
+
+// Task 4 Displaying the Products 
+function displayProducts(products) {
+    const container = document.getElementById("product-container");
+    container.innerHTML = "";
+
+//choosing the first 5 products 
+product.slice(0,5).forEach(product => {
+    const {name, price, image } = product.fields;
+
+// creating product card 
+const productCard = document.createElement("div");
+productCard.classList.add("product-card");
+
+//appending image,name and price 
+const img = document.createElement("img");
+img.src = image[1].url;
+img.alt = name;
+productCard.appendChild(img);
+
+const productName = document.createElement("h2");
+productName.textContent = name;
+productCard.appendChild(productName);
+
+const productPrice = document.createElement("p");
+productPrice.textContent = `$${(price / 1000).toFixed(2)}`;
+productCard.appendChild(productPrice);
+
+container.appendChild(productCard);
+});
+}
+
+//
